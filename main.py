c985d01583ae7e775b0f50022519524e64debb6b
RafaelNiccheri <rafaelpn1@al.insper.edu.br>
2025-06-17 09:01:18 -0300

    Acabei Finalmente graca a deus

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ load_dotenv()     # this will read .env and populate os.environ
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
 
 
 
